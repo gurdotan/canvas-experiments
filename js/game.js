cb6985a508fe7412ff340dd1d8281cdae8a6cffd
Gur Dotan <gurdotan@gmail.com>
2012-05-21 09:16:53 +0300

    Cleanup + Stop game on collision

--- js/game.js
+++ js/game.js
@@ -2,10 +2,8 @@ $(function() {
 
     var canvas = document.getElementById("playground");
     $(canvas).attr({width : screen.width, height: screen.height});
+    var screenHeight = screen.height;
     var stage;
-    var screen_width;
-    var screen_height;
-    var bmpAnimation;
     var bitmap;
     var coin;
 
@@ -33,44 +31,8 @@ $(function() {
     function startGame() {
         // create a new stage and point it at our canvas:
         stage = new Stage(canvas);
-    //    canvas.addEventListener("touchstart", function(){alert("touch started");}, false);
-    //    $(canvas).on("touchstart", function(){alert("touch started");});
-    //    $(canvas).on("touchend", function(){alert("touch end");});
-    //    $(canvas).on("touchmove", function(){alert("touch move");});
-
-        // grab canvas width and height for later calculations:
-        screen_width = canvas.width;
-        screen_height = canvas.height;
-
-        // create spritesheet and assign the associated data.
-        var spriteSheet = new SpriteSheet({
-            // image to use
-            images: [sprite],
-            // width, height & registration point of each sprite
-            frames: {width: 64, height: 64, regX: 32, regY: 32},
-            animations: {
-                walk: [0, "walk"]
-            }
-        });
-
-        // create a BitmapAnimation instance to display and play back the sprite sheet:
-    //    bmpAnimation = new BitmapAnimation(spriteSheet);
-
-        // start playing the first sequence:
-    //    bmpAnimation.gotoAndPlay("walk"); 	//animate
-
-        // set up a shadow. Note that shadows are ridiculously expensive. You could display hundreds
-        // of animated rats if you disabled the shadow.
-    //    bmpAnimation.shadow = new Shadow("#454", 0, 5, 4);
-
-    //    bmpAnimation.name = "monster1";
-    //    bmpAnimation.direction = 90;
-    //    bmpAnimation.vX = 4;
-    //    bmpAnimation.x = 16;
-    //    bmpAnimation.y = 32;
 
         // have each monster start at a specific frame
-    //    bmpAnimation.currentFrame = 0;
         bitmap = new Bitmap(sprite);
         bitmap.x = 0;
         bitmap.y = 0;
@@ -85,7 +47,6 @@ $(function() {
         // otherwise we could use Ticker.addListener(stage);
         Ticker.addListener(window);
         Ticker.useRAF = true;
-        // Best Framerate targeted (60 FPS)
         Ticker.setFPS(60);
     }
 
@@ -97,7 +58,7 @@ $(function() {
     window.tick = function() {
 
         // Hit testing the screen width, otherwise our bitmap would disappear
-        if (bitmap.y >= screen_height - sprite.height) {
+        if (bitmap.y >= screenHeight - sprite.height) {
             // We've reached the right side of our screen
             // We need to walk left now to go back to our initial position
             bitmap.direction = 0;
@@ -121,8 +82,10 @@ $(function() {
         stage.update();
         coin.tick();
 
+        // If we have a collision stop the ticker (end the game)
         if (coin.hitRadius(bitmap.x, bitmap.y, bitmap.hit)) {
-            console.log("la");
+            Ticker.removeListener(window);
+            alert("Dude!!! You just got yourself 1000 Noogra coins!  Go spend it on booze and hoes...");
         }
     };
 
