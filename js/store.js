f3dfbedd66e4e84c0f982b1d311fe71f88b87e4b
Gur Dotan <gur.dotan@capriza.com>
2012-06-22 18:20:37 +0300

    - Add store title + web font - Style the "Buy more" button - Show prices in coins instead of dollars

--- js/store.js
+++ js/store.js
@@ -69,15 +69,15 @@ $(function() {
     })
 
     var items = [
-        {title : "Colombo", price : "20.00$", img : "colombo.jpg", description : "Build stamina on each critter you capture."},
-        {title : "Fireflower", price : "15.00$", img : "fireflower.jpg", description : "Shoot enemies with the powerful flower stinger."},
-        {title : "Gloo-Boy", price : "16.00$", img : "glooboy.png", description : "Get ultra sticky feet for climbing those high branches."},
-        {title : "Jimbakoo", price : "19.00$", img : "jimbakoo.jpg", description : "Transform yourself to another random gecko."},
-        {title : "VelociDude", price : "20.00$", img : "velocidude.jpg", description : "Run like the wind with speed bursts."},
-        {title : "Zeebster", price : "15.00$", img : "zeebster.jpg", description : "Hide from snakes when crawling in the bush."},
-        {title : "Forester", price : "16.00$", img : "forester.jpg", description : "Camouflage yourself in the wake of jungle enemies"},
-        {title : "Geckstock", price : "16.00$", img : "geckstock.jpg", description : "Sooth your enemies to sleep with a groovy sixties look."},
-        {title : "Tugalu", price : "16.00$", img : "tugalu.png", description : "Gain treetop elevation with huge jumps."}
+        {title : "Colombo", price : "20", img : "colombo.jpg", description : "Build stamina on each critter you capture."},
+        {title : "Fireflower", price : "20", img : "fireflower.jpg", description : "Shoot enemies with the powerful flower stinger."},
+        {title : "Gloo-Boy", price : "20", img : "glooboy.png", description : "Get ultra sticky feet for climbing those high branches."},
+        {title : "Jimbakoo", price : "35", img : "jimbakoo.jpg", description : "Transform yourself to another random gecko."},
+        {title : "VelociDude", price : "40", img : "velocidude.jpg", description : "Run like the wind with super-fast nitro speed bursts."},
+        {title : "Zeebster", price : "40", img : "zeebster.jpg", description : "Hide from snakes when crawling in the bush."},
+        {title : "Forester", price : "60", img : "forester.jpg", description : "Camouflage yourself in the wake of jungle enemies"},
+        {title : "Geckstock", price : "60", img : "geckstock.jpg", description : "Sooth your enemies to sleep with a groovy sixties look."},
+        {title : "Tugalu", price : "65", img : "tugalu.png", description : "Gain treetop elevation with huge jumps."}
     ];
 
     var itemCollection = new ItemCollection(items);
